d2ec48e1831c5b1c32a74644a8d601d2dd8e9261
MOLYHECI <molyheci@outlook.com>
2025-07-13 16:29:33 +0800

    add Registry & Lazyloader

--- dataflow/operators/filter/__init__.py
+++ dataflow/operators/filter/__init__.py
@@ -1,17 +1,10 @@
-import sys
-from dataflow.utils.registry import LazyLoader
-from .Reasoning import *
-from .GeneralText import *
-from .AgenticRAG import *
-cur_path = "dataflow/operators/process/"
-_import_structure = {
-    "AnswerFormatterFilter": (cur_path + "Reasoning/answer_formatter_filter.py", "AnswerFormatterFilter"),
-    "AnswerGroundTruthFilter": (cur_path + "Reasoning/answer_groundtruth_filter.py", "AnswerGroundTruthFilter"),
-    "AnswerJudger_Mathverify": (cur_path + "Reasoning/answer_judger_mathverify.py", "AnswerJudger_Mathverify"),
-    "AnswerNgramFilter": (cur_path + "Reasoning/answer_ngram_filter.py", "AnswerNgramFilter"),
-    "AnswerPipelineRoot": (cur_path + "Reasoning/answer_pipeline_root.py", "AnswerPipelineRoot"),
-    "AnswerTokenLengthFilter": (cur_path + "Reasoning/answer_token_length_filter.py", "AnswerTokenLengthFilter"),
-    "QuestionFilter": (cur_path + "Reasoning/question_filter.py", "QuestionFilter"),
-}
+from typing import TYPE_CHECKING
 
-sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/process/", _import_structure)
+if TYPE_CHECKING:
+    pass # REMOVE it when you add the first operator plz!!
+else:
+    import sys
+    from dataflow.utils.registry import LazyLoader, generate_import_structure_from_type_checking
+    cur_path = "dataflow/operators/filter/"
+    _import_structure = generate_import_structure_from_type_checking(__file__, cur_path)
+    sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/filter/", _import_structure)
--- dataflow/operators/refine/__init__.py
+++ dataflow/operators/refine/__init__.py
@@ -1,9 +1,11 @@
-import sys
-from dataflow.utils.registry import LazyLoader
-from .GeneralText import *
-cur_path = "dataflow/operators/refine/"
-_import_structure = {
-    "HtmlUrlRemoverRefiner": (cur_path + "GeneralText" + "html_remove_refiner.py", "HtmlUrlRemoverRefiner")
-}
+from typing import TYPE_CHECKING
 
-sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/refine/", _import_structure)
+if TYPE_CHECKING:
+    pass
+else:
+    import sys
+    from dataflow.utils.registry import LazyLoader, generate_import_structure_from_type_checking
+
+    cur_path = "dataflow/operators/refine/"
+    _import_structure = generate_import_structure_from_type_checking(__file__, cur_path)
+    sys.modules[__name__] = LazyLoader(__name__, cur_path, _import_structure)
