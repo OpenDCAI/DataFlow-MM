3571d9c6d1ad77d8e6b5da585bbc1b0ba93601f9
Hao Liang <hao.liang@stu.pku.edu.cn>
2025-09-02 18:43:54 +0800

    fix paths

--- test/test_image_editing.py
+++ test/test_image_editing.py
@@ -10,7 +10,7 @@ from dataflow.io import ImageIO
 class ImageGenerationPipeline():
     def __init__(self, serving_type="local", api_key="", api_url="http://123.129.219.111:3000/v1/"):
         self.storage = FileStorage(
-            first_entry_file_name="./dataflow/example/image_edit/prompts.jsonl",
+            first_entry_file_name="../dataflow/example/image_edit/prompts.jsonl",
             cache_path="./cache_local/image_edit",
             file_name_prefix="dataflow_cache_step",
             cache_type="jsonl"
@@ -19,7 +19,7 @@ class ImageGenerationPipeline():
         if serving_type == "local":
             self.serving = LocalImageGenServing(
                 image_io=ImageIO(save_path=os.path.join(self.storage.cache_path, "images")),
-                hf_model_name_or_path="/ytech_m2v5_hdd/workspace/kling_mm/Models/FLUX.1-Kontext-dev",   # "black-forest-labs/FLUX.1-Kontext-dev"
+                hf_model_name_or_path="black-forest-labs/FLUX.1-Kontext-dev",   # "black-forest-labs/FLUX.1-Kontext-dev"
                 hf_cache_dir="./cache_local",
                 hf_local_dir="./ckpt/models/",
                 Image_gen_task="imageedit",
