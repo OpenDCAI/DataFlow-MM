4e00713e1049be0fc15f641d71c1cd434b492b90
zengbohan0217 <13811753844@126.com>
2025-09-09 17:01:17 +0800

    little update

--- test/test_echo4o_w_nano.py
+++ test/test_echo4o_w_nano.py
@@ -28,7 +28,7 @@ class uni_image_gen_pipeline():
             cache_type="jsonl"
         )
 
-        self.serving = LocalImageGenServing(
+        self.t2i_local_serving = LocalImageGenServing(
             image_io=ImageIO(save_path=os.path.join(self.storage.cache_path, "condition_images")),
             batch_size=4,
             hf_model_name_or_path="/ytech_m2v5_hdd/CheckPoints/FLUX.1-dev",   # "black-forest-labs/FLUX.1-dev"
@@ -36,7 +36,7 @@ class uni_image_gen_pipeline():
             hf_local_dir="./ckpt/models/"
         )
 
-        self.serving = APIVLMServing_openai(
+        self.edit_api_serving = APIVLMServing_openai(
                 api_url=api_url,
                 model_name="gemini-2.5-flash-image-preview",               # try nano-banana
                 image_io=ImageIO(save_path=os.path.join(self.storage.cache_path, "target_images")),
@@ -44,7 +44,25 @@ class uni_image_gen_pipeline():
             )
 
         self.text_to_image_generator = PromptedImageGenerator(
-            t2i_serving=self.serving,
+            t2i_serving=self.t2i_local_serving,
             save_interval=10
+        )
+
+        self.image_edit_generator = PromptedImageEditGenerator(
+            image_edit_serving=self.edit_api_serving,
+            save_interval=10
+        )
+
+    def forward(self):
+        self.text_to_image_generator.run(
+            storage=self.storage.step(),
+            input_conversation_key="image_condition_input",
+            output_image_key="condition_images",
+        )
+        self.image_edit_generator.run(
+            storage=self.storage.step(),
+            input_image_key="condition_images",
+            input_conversation_key="image_edit_input",
+            output_image_key="target_images",
         )
 
