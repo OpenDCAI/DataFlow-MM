9c888b0bc0aa02cf881c87d043c5985447038f00
Niujunbo2002 <21376334@buaa.edu.cn>
2025-10-06 16:15:51 +0800

    video

---
+++ dataflow/operators/core_vision/__init__ copy.py
@@ -0,0 +1,27 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    # === Generate ===
+    from .generate.prompted_image_edit_generator import PromptedImageEditGenerator
+    from .generate.prompted_image_generator import PromptedImageGenerator
+    from .generate.prompted_vqa_generator import PromptedVQAGenerator
+    from .generate.video_clip_generator import VideoClipGenerator
+
+    # === Filter ===
+    from .filter.video_clip_filter import VideoClipFilter
+    from .filter.video_frame_filter import VideoFrameFilter
+    from .filter.video_info_filter import VideoInfoFilter
+    from .filter.video_scene_filter import VideoSceneFilter
+    from .filter.video_score_filter import VideoScoreFilter
+
+    # === Eval ===
+    from .eval.video_aesthetic_evaluator import VideoAestheticEvaluator
+    from .eval.video_luminance_evaluator import VideoLuminanceEvaluator
+    from .eval.video_ocr_evaluator import VideoOCREvaluator
+
+else:
+    import sys
+    from dataflow.utils.registry import LazyLoader, generate_import_structure_from_type_checking
+    cur_path = "dataflow/operators/core_vision/"
+    _import_structure = generate_import_structure_from_type_checking(__file__, cur_path)
+    sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/core_vision/", _import_structure)
---
+++ test/test_promptedvideoqa.py
@@ -0,0 +1,63 @@
+from dataflow.operators.core_vision import VideoToCaptionGenerator, VideoCaptionToQAGenerator
+from dataflow.operators.conversations import Conversation2Message
+from dataflow.serving import LocalModelVLMServing_vllm
+from dataflow.utils.storage import FileStorage
+
+class VideoVQAGenerator():
+    def __init__(self):
+        self.storage = FileStorage(
+            first_entry_file_name="/mnt/shared-storage-user/mineru2-shared/niujunbo/DataFlow-MM/dataflow/example/video/sample_data.json",
+            cache_path="./cache",
+            file_name_prefix="video_vqa",
+            cache_type="json",
+        )
+        self.model_cache_dir = './dataflow_cache'
+
+        self.vlm_serving = LocalModelVLMServing_vllm(
+            # hf_model_name_or_path="/data0/models/Qwen2.5-VL-7B-Instruct",
+            hf_model_name_or_path="/mnt/shared-storage-user/mineru2-shared/niujunbo/model/Qwen/Qwen2.5-VL-7B-Instruct",
+            hf_cache_dir=self.model_cache_dir,
+            vllm_tensor_parallel_size=1,
+            vllm_temperature=0.7,
+            vllm_top_p=0.9, 
+            vllm_max_tokens=512,
+            vllm_gpu_memory_utilization=0.9
+        )
+
+        self.video_to_caption_generator = VideoToCaptionGenerator(
+            vlm_serving = self.vlm_serving,
+        )
+        self.videocaption_to_qa_generator = VideoCaptionToQAGenerator(
+            vlm_serving = self.vlm_serving,
+        )
+
+    def forward(self):
+        # Initial filters
+        # self.format_converter.run(
+        #     storage= self.storage.step(),
+        #     input_conversation_key="conversation",
+        #     output_message_key="messages",
+        # )
+
+        self.video_to_caption_generator.run(
+            storage = self.storage.step(),
+            input_image_key="image",
+            input_video_key="video",
+            input_audio_key="audio",
+            input_conversation_key="conversation",
+            output_key="caption",
+        )
+
+        self.videocaption_to_qa_generator.run(
+            storage = self.storage.step(),
+            input_image_key="image",
+            input_video_key="video",
+            input_audio_key="audio",
+            input_conversation_key="conversation",
+            output_key="qa",
+        )
+
+if __name__ == "__main__":
+    # This is the entry point for the pipeline
+    model = VideoVQAGenerator()
+    model.forward()
