d50de7f02b30a82e442df6ab152ef9420453ac77
chawuciren11 <2216740116@qq.com>
2026-02-21 10:14:33 +0800

    1

--- dataflow/statics/pipelines/api_pipelines/image_region_caption_api_pipeline.py
+++ dataflow/statics/pipelines/api_pipelines/image_region_caption_api_pipeline.py
@@ -1,10 +1,6 @@
 import os
 os.environ["DF_API_KEY"] = "sk-xxxx"
 
-<<<<<<< HEAD
-=======
-import argparse
->>>>>>> 59b89e3c5635df9109a0ece2565ac7dc4562ea1d
 from dataflow.operators.core_vision.generate.image_bbox_generator import (
     ImageBboxGenerator, 
     ExistingBBoxDataGenConfig
@@ -24,10 +20,6 @@ class ImageRegionCaptionPipeline:
         cache_type: str = "jsonl",
         input_image_key: str = "image",
         input_bbox_key: str = "bbox",
-<<<<<<< HEAD
-=======
-        image_with_bbox_path: str = 'image_with_bbox',
->>>>>>> 59b89e3c5635df9109a0ece2565ac7dc4562ea1d
         max_boxes: int = 10,
         output_image_with_bbox_path: str = "./cache/image_region_caption/image_with_bbox_result.jsonl",
     ):
@@ -62,10 +54,6 @@ class ImageRegionCaptionPipeline:
         self.caption_generator = PromptedVQAGenerator(serving=self.vlm_serving,system_prompt="You are a helpful assistant.")
         self.input_image_key = input_image_key
         self.input_bbox_key = input_bbox_key
-<<<<<<< HEAD
-=======
-        self.image_with_bbox_path=image_with_bbox_path
->>>>>>> 59b89e3c5635df9109a0ece2565ac7dc4562ea1d
         self.bbox_record=None
 
     def forward(self):
@@ -83,31 +71,5 @@ class ImageRegionCaptionPipeline:
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     pipe = ImageRegionCaptionPipeline()
-=======
-    parser = argparse.ArgumentParser(description="Image region caption with DataFlow")
-    parser.add_argument("--first_entry_file", default="./data/image_region_caption/image_region_caption_demo.jsonl")
-    parser.add_argument("--cache_path", default="./cache/image_region_caption")
-    parser.add_argument("--file_name_prefix", default="region_caption")
-    parser.add_argument("--cache_type", default="jsonl")
-    parser.add_argument("--input_image_key", default="image")
-    parser.add_argument("--input_bbox_key", default="bbox")
-
-    parser.add_argument("--max_boxes", type=int, default=10)
-    parser.add_argument("--output_image_with_bbox_path", default="./cache/image_region_caption/image_with_bbox_result.jsonl")
-
-    args = parser.parse_args()
-
-    pipe = ImageRegionCaptionPipeline(
-        first_entry_file=args.first_entry_file,
-        cache_path=args.cache_path,
-        file_name_prefix=args.file_name_prefix,
-        cache_type=args.cache_type,
-        input_image_key=args.input_image_key,
-        input_bbox_key=args.input_bbox_key,
-        max_boxes=args.max_boxes,
-        output_image_with_bbox_path=args.output_image_with_bbox_path,
-    )
->>>>>>> 59b89e3c5635df9109a0ece2565ac7dc4562ea1d
     pipe.forward()
