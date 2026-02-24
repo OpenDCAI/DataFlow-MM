e15812390f5bb499a06e337a3e8daa5c0e24dea8
chawuciren11 <2216740116@qq.com>
2026-02-24 17:09:44 +0800

    11

--- dataflow/statics/pipelines/gpu_pipelines/image_region_caption_pipeline.py
+++ dataflow/statics/pipelines/gpu_pipelines/image_region_caption_pipeline.py
@@ -15,14 +15,14 @@ class ImageRegionCaptionPipeline:
         model_path: str = "Qwen/Qwen2.5-VL-3B-Instruct",
         hf_cache_dir: str = "~/.cache/huggingface",
         download_dir: str = "./ckpt/models",
-        first_entry_file: str = "./data/image_region_caption/image_region_caption_demo.jsonl",
-        cache_path: str = "./cache/image_region_caption",
+        first_entry_file: str = "../example_data/image_region_caption/image_region_caption_demo.jsonl",
+        cache_path: str = "../cache/image_region_caption",
         file_name_prefix: str = "region_caption",
         cache_type: str = "jsonl",
         input_image_key: str = "image",
         input_bbox_key: str = "bbox",
         max_boxes: int = 10,
-        output_image_with_bbox_path: str = "./cache/image_region_caption/image_with_bbox_result.jsonl",
+        output_image_with_bbox_path: str = "../cache/image_region_caption/image_with_bbox_result.jsonl",
     ):
         self.bbox_storage = FileStorage(
             first_entry_file_name=first_entry_file,
